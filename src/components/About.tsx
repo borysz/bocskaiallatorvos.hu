c682cdf2976a98ac2dd6804c6aaf6892f7e13e13
Borysz <assessia@gmail.com>
2025-10-05 21:15:15 +0200

    Update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,13 +22,19 @@ export default function About() {
           <div>
             <h2 className="text-4xl font-bold text-gray-800 mb-6">Rólunk</h2>
             <p className="text-lg text-gray-600 mb-6 leading-relaxed">
-              Rendelőnk több mint 15 éve működik, ahol a legmodernebb eszközökkel és
-              módszerekkel segítjük kedvencei egészségének megőrzését. Szakembereink folyamatosan
-              képzik magukat, hogy a legfrissebb állatorvosi tudást alkalmazzák.
+            A Bocskai Állategészségügyi Centrumot és Állatpatikát 2019-ben nyitottuk meg a Bocskaikertben, Debrecentől 8km-re. Vállalkozásunk fő célja volt, hogy minél szélesebb körű ellátást biztosítsunk a környék állattartóinak. Fő profilunk a kutyák és macskák állategészségügyi ellátása.
             </p>
             <p className="text-lg text-gray-600 mb-8 leading-relaxed">
-              Hiszünk abban, hogy minden állat megérdemli a legjobb ellátást. Barátságos
-              környezetünkben mindent megteszünk, hogy kedvence és Ön is kényelmesen érezze magát.
+            Rendelőnk két vizsgálóval, tágas és kényelmes váróteremmel, röntgennel, ultrahangos helyiséggel, műtővel, saját laborral és állatpatikával rendelkezik. Nyugodt, tiszta környezetben, sokéves klinikai tapasztalattal és előre egyeztetett időpont alapján várjuk ügyfeleinket.
+            </p>
+            <p className="text-lg text-gray-600 mb-8 leading-relaxed">
+            Filozófiánk a dolgok mélyére látni és folyamatosan fejlődni, valamint ügyfeleinkhez maximális empátiával állni. 
+            </p>
+
+            <h2 className="text-4xl font-bold text-gray-800 mb-6">Képzett orvosok<br /><span className="text-lg text-gray-800 mb-6">sok éves tapasztalattal</span></h2>
+            
+            <p className="text-lg text-gray-600 mb-6 leading-relaxed">
+            Rendelőnk két vizsgálóval, tágas és kényelmes váróteremmel, röntgennel, ultrahangos helyiséggel, műtővel, saját laborral és állatpatikával rendelkezik. Nyugodt, tiszta környezetben, sokéves klinikai tapasztalattal és előre egyeztetett időpont alapján várjuk ügyfeleinket.
             </p>
 
             <div className="grid grid-cols-2 gap-6">
